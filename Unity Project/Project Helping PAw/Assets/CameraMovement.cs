e23cc79ffbac8bf79e64cd889a1e1a1d2b9dee51
StereoJunkie <adealdrey1@gmail.com>
2019-09-05 12:33:35 +0200

    Added camera panning

---
+++ Unity Project/Project Helping PAw/Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private Vector3 touchStart;
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if(touch.phase == TouchPhase.Began)
+                touchStart = Camera.main.ScreenToWorldPoint(touch.position);
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(touch.position);
+                Camera.main.transform.position += direction;
+            }
+        }
+    }
+}
